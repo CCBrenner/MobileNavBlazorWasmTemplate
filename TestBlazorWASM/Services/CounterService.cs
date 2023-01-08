cfecd45d349c6f78a6c3234fe5fe0aa41a63529f
Collyn Christopher Brenner <collynbrenner@gmail.com>
2023-01-08 13:17:28 -0600

    initial file tree commit

---
+++ TestBlazorWASM/Services/CounterService.cs
@@ -0,0 +1,14 @@
+﻿using TestBlazorWASM.Services.Contracts;
+
+namespace TestBlazorWASM.Services
+{
+    public class CounterService : ICounterService
+    {
+        public int Count { get; set; } = 0;
+        public int Amount { get; set; } = 1;
+
+        public void IncrementCount() => Count += Amount;
+        public void DecrementCount() => Count -= Amount;
+        public void ResetCount() => Count = 0;
+    }
+}
